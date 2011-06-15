0cc0f37aa88fd09c9a035e62fb7248cffbd581ae
Emma Kuo <emma.kuo@intel.com>
2011-06-16 00:42:24 +0100

    move i18n strings to a better location

---
+++ strings.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011 Intel Corporation.
+ *
+ * This program is licensed under the terms and conditions of the
+ * Apache License, version 2.0.  The full text of the Apache License is at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+var string01 = qsTr("You cannot add more than %1 clocks", "clock creation error");
+
+var string02 = qsTr("You have no alarms", "alarms page, no content");
+var string03 = qsTr("Create a new alarm", "alarms page, no content");
+var string04 = qsTr("You cannot add more than %1 alarms", "alarm creation error");
+var string05 = qsTr("You must provide a name", "alarm creation error");
+var string06 = qsTr("You must select at least one day", "alarm creation error");
+
+var string07 = qsTr("Timers", "page title");
+var string08 = qsTr("You have no timers", "timers page, no content");
+var string09 = qsTr("Create a new timer", "timers page, no cotnent");
+var string10 = qsTr("New timer", "menu item");
+var string11 = qsTr("Add new timer", "timer creation dialogbox title");
+var string12 = qsTr("You cannot add more than %1 timers", "timer creation error");
+var string13 = qsTr("You must provide a name", "timer creation error");
+var string14 = qsTr("Delete timer", "timer deletion dialogbox title");
+var string15 = qsTr("Are you sure you want to delete?", "timer deletion dialogbox text");
+var string16 = qsTr("Duration", "timer settings");
+var string17 = qsTr("Hours", "timer settings");
+var string18 = qsTr("Minutes", "timer settings");
+var string19 = qsTr("Seconds", "timer settings");
+var string20 = qsTr("Start", "timer button");
+var string21 = qsTr("Pause", "timer button");
+var string22 = qsTr("min", "timer display (min=minutes)");
+var string23 = qsTr("remaining", "timer display");
+var string24 = qsTr("Cancel", "timer button");
+
+var string25 = qsTr("Stopwatch", "page title");
+var string26 = qsTr("hours", "stopwatch display");
+var string27 = qsTr("min", "stopwatch display (min=minutes)");
+var string28 = qsTr("sec", "stopwatch display (sec=seconds)");
+var string29 = qsTr("ms", "stopwatch display (ms=milliseconds)");
+var string30 = qsTr("Start", "stopwatch button");
+var string31 = qsTr("Stop", "stopwatch button");
+var string32 = qsTr("Create lap", "stopwatch button");
+var string33 = qsTr("Reset", "stopwatch button");
+var string34 = qsTr("Lap %1", "stopwatch label");
